0ead19b759f4a90bce92a1743ddcd02705ccd650
Alex Luce <alexanderluce@gmail.com>
2021-11-18 14:51:46 -0500

    u

--- contracts/ZeroDAO.sol
+++ contracts/ZeroDAO.sol
@@ -5,8 +5,25 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
 contract ZeroDAO is Initializable, ERC20Upgradeable {
+
+    address mintAuthority;
+
+    modifier onlyMintAuthority {
+        require(msg.sender == mintAuthority, '!mintAuthority');
+        _;
+    }
+
     function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
         __ERC20_init(name, symbol);
+        mintAuthority = msg.sender;
         _mint(_msgSender(), initialSupply);
+    }
+
+    function mint(address _account, uint256 _amount) public onlyMintAuthority {
+        _mint(_account, _amount);
+    }
+
+    function setMintAuthority(address _account) public onlyMintAuthority {
+        mintAuthority = _account;
     }
 }
--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -1,6 +1,43 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-const ZeroDAO = artifacts.require('ZeroDAO');
+const { deployProxy, deployer } = require('@openzeppelin/truffle-upgrades');
+
+/*
+CONSTANTS
+*/
+BLOCK_REWARDS = 10000000000000
+START_BLOCK = 21526489
+END_BLOCK = 21526490
+
+const deployFixedAddress = async (...args) => {
+    console.log('Deploying ' + args[0]);
+    args[1].waitConfirmations = 1;
+    const [signer] = await ethers.getSigners();
+    const result = await deployments.deploy(...args);
+    console.log('Deployed to ' + result.address);
+    return result;
+  };
+
+const deployProxyFixedAddress = async (...args) => {
+    console.log('Deploying proxy');
+    const result = await upgrades.deployProxy(...args);
+    return result;
+};
 
 module.exports = async function (deployer) {
-    await deployProxy(ZeroDAO, ['zeroDAO', 'ZERO', '88000000000000000000000000'], { deployer, initializer: 'initialize' });
+    const [signer] = await ethers.getSigners();
+
+    // Deploy the zero token
+    const zeroDAO = await deployProxyFixedAddress('ZeroDAO', {
+        contractName: 'ZeroDAO',
+        args: ['zeroDAO', 'ZERO', '88000000000000000000000000'],
+        from: deployer
+    });
+
+    // Deploy the master chef contract
+    const masterChef = await deployFixedAddress('MasterChef', {
+        contractName: 'MasterChef',
+        args: [zeroDAO.address, signer.address, BLOCK_REWARDS, START_BLOCK, END_BLOCK],
+        from: deployer
+    });
+
+    await zeroDAO.setMintAuthority(masterChef.address).wait()
 };
